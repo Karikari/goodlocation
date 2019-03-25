8dbb59aee3033a29958dfcb36b2b9c67b8809c36
Kwaku Karikari <kwakukarikari88@gmail.com>
2019-03-25 16:49:36 +0000

    added getlastknow location by fuse location api

--- app/src/main/java/com/karikari/goodlocation/MainActivity.java
+++ app/src/main/java/com/karikari/goodlocation/MainActivity.java
@@ -2,14 +2,42 @@ package com.karikari.goodlocation;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.location.Location;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = MainActivity.class.getSimpleName();
+
+    GoodLocation goodLocation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        goodLocation = new GoodLocation(this);
+
+        /*goodLocation.autoStartLocation(new GoodLocation.GoodLocationListener() {
+            @Override
+            public void onCurrenLocation(Location location) {
+                Log.d(TAG, "Lat : "+ location.getLatitude() + " Lng : "+ location.getLongitude());
+            }
+
+            @Override
+            public void onError(String error) {
+                Log.d(TAG, "Error : "+ error);
+            }
+        });*/
+
+        Location location= goodLocation.getLastKnownLocation();
+
+        if(location!=null){
+            Log.d(TAG, "Lat : "+ location.getLatitude() + " Lng : "+ location.getLongitude());
+        }else {
+            Log.d(TAG, "Location is Null");
+        }
 
 
 
